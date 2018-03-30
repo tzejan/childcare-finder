b7627403bd4cf4c9018b28938fc7f67866ca3196
Sim Tze Jan <tzejan@gmail.com>
2018-03-30 23:50:01 +0800

    Updated test to pass

--- src/tests/map/CentrePopUp.test.js
+++ src/tests/map/CentrePopUp.test.js
@@ -13,12 +13,12 @@ describe("CentrePopUp test", () => {
     it("should have popup and CentreInfo created", () => {
       let testData = { longitude: 103.894482346, latitude: 1.38975552500006 };
       const wrapper = shallow(<CentrePopup data={testData} />);
-
+      
       expect(wrapper.children("Popup").exists()).toEqual(true);
       expect(
         wrapper
           .children("Popup")
-          .children("CentreInfo")
+          .children("WithStyles(CentreInfo)")
           .exists()
       ).toEqual(true);
     });
